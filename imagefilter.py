d33f99c0f48e3b2f33b33944e31297ed565ab341
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-02-14 10:04:00 +0000

    Grayscale

--- imagefilter.py
+++ imagefilter.py
@@ -1,3 +1,4 @@
+import math
 # run pip install pillow to install
 from PIL import Image
 
@@ -17,12 +18,16 @@ def main():
     # create a new image of the same size as the original
     new_image = Image.new("RGB", (image.size), "white")
 
-    # place a pixel from the original image into the new image
-    new_image.putpixel((50, 50), (r, g, b))
-
     # open the new image
     new_image.show()
 
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x,y))
+            gray = new_imageputpixel((x,y), (math.floor((r + g + b) / 3)))
+            new_image.putpixel((x,y)(gray, gray, gray))
+    
+    new_image.save("grayscale.JPG")
 
 if __name__ == "__main__":
     main()
