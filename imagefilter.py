c866b15c8aa6b58b5dc405fd7ae0cb679f15e5ec
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-02-21 06:51:19 +0000

    switch

--- imagefilter.py
+++ imagefilter.py
@@ -1,4 +1,5 @@
 import math
+import sys
 # run pip install pillow to install
 from PIL import Image
 
@@ -21,13 +22,48 @@ def main():
     # open the new image
     new_image.show()
 
-    for x in range(width):
-        for y in range(height):
-            r, g, b = image.getpixel((x,y))
-            gray = new_imageputpixel((x,y), (math.floor((r + g + b) / 3)))
-            new_image.putpixel((x,y)(gray, gray, gray))
+    def grayscale(height, width):
+        for x in range(width):
+            for y in range(height):
+                r, g, b = image.getpixel((x,y))
+                gray = new_image.putpixel((x,y), (math.floor((r + g + b) / 3)))
+                new_image.putpixel((x,y)(gray, gray, gray))
+        new_image.save("grayscale.JPG")
+
+    def switch(height, width):
+        for x in range(width):
+            for y in range(height):
+                # Get the pixel from the original image
+                r, g, b = image.getpixel((x,y))
+                # Set the pixel in the new image
+                new_image.putpixel((width-1-x, y), (r, g, b))
+            
+            # Show and save the new image
+        new_image.show()
+        new_image.save("switch.JPG")
+                
+    def flip(height, width):
+        for x in range(width):
+            for y in range(height):
+                # Get the pixel from the original image
+                r, g, b = image.getpixel((x,y))
+                # Set the pixel in the new image
+                new_image.putpixel((x, height-1-y), (r, g, b))
+
+        # Show and save the new image
+        new_image.show()
+        new_image.save("flip.JPG")
+
+    mode = sys.argv[1]
+
+    if mode == "gray":
+        grayscale(height, width)
     
-    new_image.save("grayscale.JPG")
+    if mode == "switch":
+        switch(height, width)
+    
+    if mode == "flip":
+        flip(height, width)
 
 if __name__ == "__main__":
     main()
