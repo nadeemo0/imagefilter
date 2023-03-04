f69d66dd14302b8e02b02206ec99486ebe4afd3a
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-03-04 21:07:45 +0000

    Added shrink and changed main

--- imagefilter.py
+++ imagefilter.py
@@ -1,7 +1,7 @@
 import math
-import sys
-# run pip install pillow to install
 from PIL import Image
+
+VALID_MODES = ['gray', 'flip', 'shrink']
 
 def main():
     # Open image
@@ -10,60 +10,67 @@ def main():
     # Show image
     image.show()
 
-    # get the height and width
+    # Get the height and width
     width, height = image.size
 
-    # get the rgb values of a pixel at a certain coordinate
-    r, g, b = image.getpixel((50, 50))
-    
-    # create a new image of the same size as the original
-    new_image = Image.new("RGB", (image.size), "white")
+    # Create a new image of the same size as the original
+    new_image = Image.new("RGB", (width, height), "white")
 
-    # open the new image
+    mode = input("Enter mode: ")
+
+    if mode in VALID_MODES:
+        if mode == "gray":
+            grayscale(image, new_image)
+        elif mode == "flip":
+            m2 = input("Vertical or Horizontal?\n")
+            if m2 == "Vertical":
+                flip("Vertical", image, new_image)
+            elif m2 == "Horizontal":
+                flip("Horizontal", image, new_image)
+        elif mode == "shrink":
+            shrink(image, new_image)
+    else:
+        print("Invalid mode")
+
+def grayscale(image, new_image):
+    # Convert image to grayscale
+    for x in range(image.width):
+        for y in range(image.height):
+            r, g, b = image.getpixel((x,y))
+            gray = math.floor((r + g + b) / 3)
+            new_image.putpixel((x,y), (gray, gray, gray))
     new_image.show()
+    new_image.save("grayscale.JPG")
 
-    def grayscale(height, width):
-        for x in range(width):
-            for y in range(height):
-                r, g, b = image.getpixel((x,y))
-                gray = new_image.putpixel((x,y), (math.floor((r + g + b) / 3)))
-                new_image.putpixel((x,y)(gray, gray, gray))
-        new_image.save("grayscale.JPG")
+def flip(direction, image, new_image):
+    # Flip image 
+    for x in range(image.width):
+        for y in range(image.height):
+            r, g, b = image.getpixel((x,y))
+            if direction == "Vertical": 
+                new_image.putpixel((x, image.height-1-y), (r, g, b))
+            elif direction == "Horizontal":
+                new_image.putpixel((image.width-1-x, y), (r, g, b))
+    new_image.show()
+    new_image.save("flip.JPG")
 
-    def switch(height, width):
-        for x in range(width):
-            for y in range(height):
-                # Get the pixel from the original image
-                r, g, b = image.getpixel((x,y))
-                # Set the pixel in the new image
-                new_image.putpixel((width-1-x, y), (r, g, b))
-            
-            # Show and save the new image
-        new_image.show()
-        new_image.save("switch.JPG")
-                
-    def flip(height, width):
-        for x in range(width):
-            for y in range(height):
-                # Get the pixel from the original image
-                r, g, b = image.getpixel((x,y))
-                # Set the pixel in the new image
-                new_image.putpixel((x, height-1-y), (r, g, b))
+def shrink(image, new_image):
+    # Calculate new width and height
+    new_width = image.width // 2
+    new_height = image.height // 2
 
-        # Show and save the new image
-        new_image.show()
-        new_image.save("flip.JPG")
+    # Create new image with half the pixels
+    new_image = Image.new("RGB", (new_width, new_height), "white")
 
-    mode = sys.argv[1]
+    # Copy every other pixel from original image to new image
+    for x in range(new_width):
+        for y in range(new_height):
+            r, g, b = image.getpixel((2*x, 2*y))
+            new_image.putpixel((x,y), (r, g, b))
 
-    if mode == "gray":
-        grayscale(height, width)
-    
-    if mode == "switch":
-        switch(height, width)
-    
-    if mode == "flip":
-        flip(height, width)
+    # Show and save new image
+    new_image.show()
+    new_image.save("shrink.JPG")
 
 if __name__ == "__main__":
     main()
